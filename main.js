3e9b6b59718e245abb406fa6bc7a88053ded535c
yoshi <yoshi@tech-camp-9-021-no-MacBook-Air.local>
2017-05-22 17:44:25 +0900

    jquery基本勉強終了

--- main.js
+++ main.js
@@ -29,14 +29,31 @@ $(function() {
   });
 
   // add classボタンを押したとき
+  // クラスを追加しよう
   $("#addClassButton").on("click", function() {
     $("p").addClass("red");
   });
 
   // remove classボタンを押したとき
+  //クラスを削除しよう
   $("#removeClassButton").on("click", function() {
     $("p").removeClass("red");
   });
+
+  //属性の値を取得しよう
+  $("#getAttrButton").on("click",function(){
+    console.log($("img").attr("src"));
+  });
+
+  //属性の値を変更してみよう
+  $("#changeAttrButton").on("click",function(){
+    console.log($("img").attr("src","http://yoso-walk.net/wp-content/uploads/2014/07/enhanced-buzz-wide-6868-1401738537-9_R.jpg"));
+  });
+
+  //フォームの値を取得しよう
+ $("#valButton").on("click",function(){
+    console.log($("input").val());
+ });
 
 
 });
